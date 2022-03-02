db01e1b82646743145f6f1202608c4573c074fa0
Jurgen hace programas <jurgenfbermudez@gmail.com>
2022-03-01 19:15:25 -0600

    Add Abouts

--- app/src/main/java/com/jurgenbermudez/sl/AboutUsFragment.java
+++ app/src/main/java/com/jurgenbermudez/sl/AboutUsFragment.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 public class AboutUsFragment extends Fragment {
 
@@ -20,6 +21,12 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view =inflater.inflate(R.layout.fragment_about_us, container, false);
+
+        //TextView textView = view.findViewById(R.id.txt_get_link);
+        //textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return view;
+
     }
 }
